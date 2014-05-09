9d6711e6b6593ac612aab24cb8ee720840cc883c
Helmut Wollmersdorfer <helmut.wollmersdorfer@fixpunkt.de>
2014-05-09 10:13:06 +0200

    initial files

---
+++ lib/John/Doe.pm
@@ -0,0 +1,44 @@
+package John::Doe;
+
+use strict;
+use warnings;
+
+use 5.008_005;
+our $VERSION = '0.00';
+
+
+1;
+
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+John::Doe - Test case duplicate file names in distribution
+
+=head1 SYNOPSIS
+
+  use John::doe;
+
+=head1 DESCRIPTION
+
+Don't install or use it, it is BROKEN by intent.
+
+=head1 AUTHOR
+
+Helmut Wollmersdorfer E<lt>helmut.wollmersdorfer@gmx.atE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2014- Helmut Wollmersdorfer
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
+
